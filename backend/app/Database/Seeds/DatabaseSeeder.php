14e8df97494a93f9f3ad9cdde7abe60b60a8f3f7
amiliadanielle <amiliadanielle06@gmail.com>
2025-10-30 17:40:58 +0800

    refactor(db/seeds): update DatabaseSeeder to extend UsersSeeder and enhance docblock comments

--- backend/app/Database/Seeds/DatabaseSeeder.php
+++ backend/app/Database/Seeds/DatabaseSeeder.php
@@ -4,11 +4,21 @@ namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+/**
+ * DatabaseSeeder
+ *
+ * Docblock hints help Intelephense (VS Code) understand methods/properties
+ * provided by the CodeIgniter Seeder base class when the framework isn't indexed.
+ *
+ * @method void call(string $class)
+ */
+class DatabaseSeeder extends UsersSeeder
 {
     public function run()
     {
+        // call other seeders
         $this->call('App\\Database\\Seeds\\ClearDatabaseSeeder');
+        $this->call('App\\Database\\Seeds\\UsersSeeder');
 
         // $this->call('App\\Database\\Seeds\\<name of the seeder here>');
     }
